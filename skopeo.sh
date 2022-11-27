429435e5b095558713d02b57cd3b2cd6c3f601f7
cby-chen <cby@chenby.cn>
2022-11-27 13:01:09 +0000

    update

--- skopeo.sh
+++ skopeo.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 hub="swr.cn-north-1.myhuaweicloud.com"
+repo="$hub/chenby"
 
-if [ -f config.yaml ]; then
+rm -rf images.yaml
+bash config.sh
 
-    for images in $(cat config.yaml); do
-        echo "[Start] sync  ${images} ......."
-
-        sudo skopeo login --tls-verify=false -u ${HUB_USERNAME} -p ${HUB_PASSWORD} ${hub} \
-        && sudo skopeo sync --src-tls-verify=false --dest-tls-verify=false --insecure-policy --src docker --dest docker ${images} ${hub}/chenby/
-
-        echo "[End] ${images} done."
-    done
-
+if [ -f images.yaml ]; then
+   echo "[Start] sync......."
+   
+   sudo skopeo login --tls-verify=false -u ${HUB_USERNAME} -p ${HUB_PASSWORD} ${hub} \
+   && sudo skopeo --tls-verify=false --insecure-policy sync --src yaml --dest docker images.yaml $repo
+   
+   echo "[End] done."
+   
 else
-    echo "[Error]not found config.yaml!"
+    echo "[Error]not found images.yaml!"
 fi
-
